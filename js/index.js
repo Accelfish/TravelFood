cef1ba84aed6723bb641bf1c62f9414e8a91b653
adam_yu <adam_yu@cmoney.com.tw>
2021-08-11 11:47:22 +0800

    modify: 後修
    1. 統一使用物件設定的數值
    2. 資料改為二維陣列減少渲染時的計算
    3. 換迴圈的方法
    4. 修正頁碼變換時跑全部頁碼DOM的問題
    5. 頁碼值改用 data-* 設定
    6. 更正 js 觸發的 class 的命名

--- js/index.js
+++ js/index.js
@@ -2,190 +2,199 @@ class TravelFood {
   constructor() {
     this.pageObject = {
       currentPage: 0, //目前頁碼
-      totalPage: 0,   //總頁數
-      pageSize: 10,   //每頁呈現的資料列
+      totalPage: 0, //總頁數
+      pageSize: 10, //每頁呈現的資料列
       travelFood: [], //呈現的資料來源(原始資料)
     };
     this.init();
   }
 
-  init() {    
-    this.loading = document.querySelector("#Loading");
-    this.travelFoodList = document.querySelector("#TfBody");
-    this.pagination = document.querySelector("#Pagination");
+  init() {
+    this.loading = document.querySelector('#Loading');
+    this.travelFoodList = document.querySelector('#TfBody');
+    this.pagination = document.querySelector('#Pagination');
     this.callTravelFoodData();
   }
 
-	/**
-	 * 取得總頁數
-	 * @returns 總頁數
-	 */
+  /**
+   * 取得總頁數
+   * @returns 總頁數
+   */
   getTotalPage() {
     return this.pageObject.totalPage;
   }
-	
-	/**
-	 * 取得目前頁碼
-	 * @returns 目前的頁碼
-	 */
-	getCurrentPage(){
-		return this.pageObject.currentPage;
-	}
 
-	/**
-	 * 取得已儲存的小吃資料
-	 * @returns 已儲存的小吃資料
-	 */
-	getTravelFoodData(){
-		return this.pageObject.travelFood;
-	}
+  /**
+   * 取得目前頁碼
+   * @returns 目前的頁碼
+   */
+  getCurrentPage() {
+    return this.pageObject.currentPage;
+  }
 
-	/**
-	 * 設定目前頁碼與列表
-	 * @param {number} targetPage 
-	 */
+  /**
+   * 取得每頁的資料數
+   * @returns 每頁的資料筆數
+   */
+  getPageSize() {
+    return this.pageObject.pageSize;
+  }
+
+  /**
+   * 取得已儲存的小吃資料
+   * @returns 已儲存的小吃資料
+   */
+  getTravelFoodData() {
+    return this.pageObject.travelFood;
+  }
+
+  /**
+   * 設定目前頁碼與列表
+   * @param {number} targetPage
+   */
   setCurrentPage(targetPage) {
     this.pageObject.currentPage = targetPage;
     this.renderTable();
   }
 
-	/**
-	 * 設定總頁數
-	 * @param {number}} value 
-	 */
+  /**
+   * 設定總頁數
+   * @param {number}} value
+   */
   setTotalPage(value) {
     this.pageObject.totalPage = value;
   }
 
-	/**
-	 * 設定原始資料
-	 * @param {object} value 
-	 */
+  /**
+   * 設定全部資料
+   * @param {object} value
+   */
   setTravelFood(value) {
     this.pageObject.travelFood = value;
   }
 
-	/**
-	 * 設定按下頁碼的事件
-	 */
+  /**
+   * 設定按下頁碼的事件
+   */
   setPageItemEvent() {
     let vm = this;
-    for (let i = 0; i < vm.pageItems.length; i += 1) {
-      vm.pageItems[i].addEventListener("click", function () {
-        for (let i = 0; i < vm.pageItems.length; i += 1) {
-          vm.pageItems[i].classList.remove("tf__pageItem--active");
+    const pageItemDOM = vm.pageItems;
+    for (let element of pageItemDOM) {
+      element.addEventListener('click', function () {
+        document
+          .getElementsByClassName('js-tf__pageItem')[0]
+          .classList.remove('js-tf__pageItem');
+        this.classList.add('js-tf__pageItem');
+        let pageNumber = parseInt(element.dataset.pageNumber);
+        if (pageNumber) {
+          vm.setCurrentPage(pageNumber);
         }
-        this.classList.add("tf__pageItem--active");
-        vm.setCurrentPage(this.innerText);
       });
     }
   }
 
-	/**
-	 * 渲染頁碼
-	 */
+  /**
+   * 渲染頁碼
+   */
   renderPagination() {
     let vm = this;
     let totalPage = this.getTotalPage();
-    let template = "";
-    for (let i = 1; i <= totalPage; i += 1) {
+    let template = '';
+    let i = 0;
+    while (i < totalPage) {
+      i += 1;
       if (i === 1) {
-        template += `<li class="text-center tf__pageItem tf__pageItem--active">${i}</li>`;
-      } else {
-        template += `<li class="text-center tf__pageItem">${i}</li>`;
-      }
+        template += `<li class='text-center tf__pageItem js-tf__pageItem' data-page-number='${i}'>${i}</li>`;
+        continue;
+      } 
+      template += `<li class='text-center tf__pageItem' data-page-number='${i}'>${i}</li>`;
     }
     vm.pagination.innerHTML = template;
-    vm.pageItems = document.getElementsByClassName("tf__pageItem");
+    vm.pageItems = document.getElementsByClassName('tf__pageItem');
   }
 
-	/**
-	 * 渲染表格
-	 */
+  /**
+   * 渲染表格
+   */
   renderTable() {
     let vm = this;
-    let tableTemplate = "";
-		let travelFoodData = vm.getTravelFoodData();
-		let currentPage = vm.getCurrentPage();
+    let tableTemplate = '';
+    let travelFoodData = vm.getTravelFoodData();
+    let currentPage = vm.getCurrentPage() - 1;
+    let pageSize = vm.getPageSize();
+    let currentData = travelFoodData[currentPage];
+    let previewImageAlignBottomIndex = pageSize - 2; //預覽圖片最後兩張空間靠下
 
-    //設定渲染資料的起始Index
-    let startIndex = (currentPage - 1) * vm.pageObject.pageSize + 1;
+    currentData.forEach((foodData, index) => {
+      let currentDataIndex = currentPage * pageSize + index + 1; //目前資料在總資料的index
 
-    //設定渲染資料的中止Index
-    let endIndex =
-      currentPage * vm.pageObject.pageSize > vm.pageObject.travelFood.length
-        ? vm.pageObject.travelFood.length
-        : vm.pageObject.currentPage * vm.pageObject.pageSize;
-
-    let listIndex = 0; //每頁每列的index
-    let previewImageAlignBottomIndex = 9; //預覽圖片最後兩張空間靠下
-
-    for (let i = startIndex; i <= endIndex; i += 1) {
-      let data = travelFoodData[i - 1];
-      listIndex += 1;
-
-      if (i % 2) {
-        tableTemplate += `<tr class="tf__row">`;
+      if (index % 2) {
+        tableTemplate += `<tr class='tf__row'>`;
       } else {
-        tableTemplate += `<tr class="tf__row tf__row-even">`;
+        tableTemplate += `<tr class='tf__row tf__row-even'>`;
       }
 
-      tableTemplate += `<td class="tf__td text-secondary text-center" title="${i}">${i}</td>
-						<td class="tf__td text-center" title="${data.City}">${data.City}</td>
-						<td class="tf__td" title="${data.Name}">
-							<div class="inner__image">
-								<img class="tf__image" src="${
-                  data.PicURL
-                }" loading="lazy" width="80" height="50" alt="${data.Name}">
-								<div class="inner__preview ${
-                  listIndex >= previewImageAlignBottomIndex
-                    ? "inner__preview-bottom"
-                    : ""}">
-									<img class="tf__previmage" src="${data.PicURL}" width="280" height="190" alt="${data.Name}">
+      tableTemplate += `<td class='tf__td text-secondary text-center' title='${currentDataIndex}'>${currentDataIndex}</td>
+						<td class='tf__td text-center' title='${foodData.City}'>${foodData.City}</td>
+						<td class='tf__td' title='${foodData.Name}'>
+							<div class='inner__image'>
+								<img class='tf__image' src='${foodData.PicURL}' loading='lazy' width='80' height='50' alt='${foodData.Name}'>
+								<div class='inner__preview 
+                ${index >= previewImageAlignBottomIndex
+                    ? 'inner__preview-bottom'
+                    : ''}'>
+									<img class='tf__previmage' src='${foodData.PicURL}' width='280' height='190' alt='${foodData.Name}'>
 								</div>
 							</div>
 						</td>
-						<td class="tf__td tf__td-name" title="${data.Name}">
-							${
-                data.Url
-                  ? `<a class="text__link" href=${data.Url} target='_blank' title='${data.Name}'>${data.Name}</a>`
-                  : data.Name
-              }
+						<td class='tf__td tf__td-name' title='${foodData.Name}'>
+							${foodData.Url
+                  ? `<a class='text__link' href=${foodData.Url} target='_blank' title='${foodData.Name}'>${foodData.Name}</a>`
+                  : foodData.Name}
 						</td>
-						<td class="tf__td" title="${data.HostWords}">
+						<td class='tf__td' title='${foodData.HostWords}'>
 							${
-                data.HostWords.length < 50
-                  ? data.HostWords
-                  : data.HostWords.slice(0, 50) + "..."
+                foodData.HostWords.length < 50
+                  ? foodData.HostWords
+                  : foodData.HostWords.slice(0, 50) + '...'
               }
 				</td>
 			</tr>`;
-    }
+    });
 
     vm.travelFoodList.innerHTML = tableTemplate;
   }
 
-	/**
-	 * 呼叫API取資料
-	 */
+  /**
+   * 呼叫API取資料
+   */
   callTravelFoodData() {
     let vm = this;
-    vm.loading.classList.remove("js-hidden");
-    fetch("https://data.coa.gov.tw/Service/OpenData/ODwsv/ODwsvTravelFood.aspx")
+    vm.loading.classList.remove('js-hidden');
+    fetch('https://data.coa.gov.tw/Service/OpenData/ODwsv/ODwsvTravelFood.aspx')
       .then((response) => response.json())
       .then((foodData) => {
-        vm.setTotalPage(Math.ceil(foodData.length / 10));
-        vm.setTravelFood(foodData);
+        let len = foodData.length;
+        let pageSize = vm.getPageSize();
+        vm.setTotalPage(Math.ceil(len / pageSize));
+        //整理資料為二維陣列
+        let sortData = [];
+        let index = 0;
+        while (index <= len) {
+          sortData.push(foodData.splice(0, pageSize));
+          index += pageSize;
+        }
+        vm.setTravelFood(sortData);
         vm.renderPagination();
         vm.setPageItemEvent();
         vm.setCurrentPage(1); //取完資料設定第一頁
-        document.getElementById("Pagination").classList.remove("js-hidden");
+        document.getElementById('Pagination').classList.remove('js-hidden');
       })
       .catch((error) => {
         console.error(error);
       })
       .finally(() => {
-        vm.loading.classList.add("js-hidden");
+        vm.loading.classList.add('js-hidden');
       });
   }
 }
